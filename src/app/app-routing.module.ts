50d24b52c859cd8240e12e3cff8b428118e2a5c9
dinesh <dineshbhumca16@gmail.com>
2020-08-20 09:17:31 +0530

    sencond commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Ex2Component } from './ex2/ex2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'ex2',component:Ex2Component},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
